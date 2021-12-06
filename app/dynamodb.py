a5ce5bdf1313349a426230911f86ee82b1700269
Blare <yc927596@dal.ca>
2021-12-05 21:27:59 -0500

    small fix.

--- app/dynamodb.py
+++ app/dynamodb.py
@@ -11,65 +11,65 @@ eventname = 'ece1762'
 starttime = '2021-12-04 18:00'
 endtime = '2021-12-04 19:00'
 # add new event to the end of the list
-# dynamoTable.update_item(
-#     Key={
-#         'user_name': 'Jiahao',
-#     },
-#     UpdateExpression='SET event = list_append(event, :vals)',
-#     ExpressionAttributeValues={
-#         ':vals': [{'name': eventname,
-#                    'start_time': starttime,
-#                    'end_time': endtime}]
-#     }
-# )
-# # remove event in list
-# response = dynamoTable.query(
-#     KeyConditionExpression=Key('user_name').eq('Jiahao')
-# )
-# items = response['Items']
-# event_list = items[0]['event']
-# for event in event_list:
-#     if event['name'] == 'ece1762':
-#         event_idx = event_list.index(event)
-#
-# query = 'REMOVE event[%d]" % (event_idx)'
-# dynamoTable.update_item(
-#     Key={
-#         'user_name': 'Jiahao',
-#     },
-#     UpdateExpression=query
-# )
+dynamoTable.update_item(
+    Key={
+        'user_name': 'Jiahao',
+    },
+    UpdateExpression='SET event = list_append(event, :vals)',
+    ExpressionAttributeValues={
+        ':vals': [{'name': eventname,
+                   'start_time': starttime,
+                   'end_time': endtime}]
+    }
+)
+# remove event in list
+response = dynamoTable.query(
+    KeyConditionExpression=Key('user_name').eq('Jiahao')
+)
+items = response['Items']
+event_list = items[0]['event']
+for event in event_list:
+    if event['name'] == 'ece1762':
+        event_idx = event_list.index(event)
+
+query = 'REMOVE event[%d]" % (event_idx)'
+dynamoTable.update_item(
+    Key={
+        'user_name': 'Jiahao',
+    },
+    UpdateExpression=query
+)
 
 # edit event in list
-# response = dynamoTable.query(
-#     KeyConditionExpression=Key('user_name').eq('Jiahao')
-# )
-# items = response['Items']
-# event_list = items[0]['event']
-# for event in event_list:
-#     if event['name'] == 'aps1070':
-#         event_idx = event_list.index(event)
-#
-# query = 'SET event[%d] = :vals' % (event_idx)
-# dynamoTable.update_item(
-#     Key={
-#         'user_name': 'Jiahao',
-#     },
-#     UpdateExpression=query,
-#     ExpressionAttributeValues={
-#         ':vals': {'name': eventname,
-#                    'start_time': starttime,
-#                    'end_time': endtime}
-#     }
-# )
+response = dynamoTable.query(
+    KeyConditionExpression=Key('user_name').eq('Jiahao')
+)
+items = response['Items']
+event_list = items[0]['event']
+for event in event_list:
+    if event['name'] == 'aps1070':
+        event_idx = event_list.index(event)
+
+query = 'SET event[%d] = :vals' % (event_idx)
+dynamoTable.update_item(
+    Key={
+        'user_name': 'Jiahao',
+    },
+    UpdateExpression=query,
+    ExpressionAttributeValues={
+        ':vals': {'name': eventname,
+                   'start_time': starttime,
+                   'end_time': endtime}
+    }
+)
 
 # search event in list
-# response = dynamoTable.query(
-#     KeyConditionExpression=Key('user_name').eq('Jiahao')
-# )
-# items = response['Items']
-# event_list = items[0]['event']
-# for event in event_list:
-#     if event['name'] == 'ece1762':
-#         event_idx = event_list.index(event)
-# print(event_list[event_idx])
+response = dynamoTable.query(
+    KeyConditionExpression=Key('user_name').eq('Jiahao')
+)
+items = response['Items']
+event_list = items[0]['event']
+for event in event_list:
+    if event['name'] == 'ece1762':
+        event_idx = event_list.index(event)
+print(event_list[event_idx])
